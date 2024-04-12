d098ad102d67ae9ad32bb9c357bae1d2aa9507fe
SofTest94 <165555006+SofTest94@users.noreply.github.com>
2024-04-12 07:50:00 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Este es dev - y esta es mi url de mi api de dev: {process.env.REACT_APP_NAME}</p>
+        <p>Este es dev ssss- y esta es mi url de mi api de dev: {process.env.REACT_APP_NAME}</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
